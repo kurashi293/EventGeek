4b32cbfb6d62028841966d514a091d87c8051bcb
kurashi293 <my_post_office@icloud.com>
2019-11-28 23:35:58 +0900

    deviseのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+  devise_scope :user do
+    get "/login", :to => "users/sessions#new"
+    get "/registration", :to => "users/registrations#new"
+    post "/registration/create", :to => "users/registrations#create"
+  end
+
   get "task" => "tasks#index"
   get "member" => "members#index"
   get "chat" => "chats#index"
