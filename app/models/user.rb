d94ade460f2d398676b86e653dba546aa37fefe7
kurashi293 <my_post_office@icloud.com>
2020-05-13 18:00:57 +0900

    バリデーションを実装
    ユーザー新規作成時のバリデーション を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,33 +12,19 @@ class User < ApplicationRecord
   has_one  :status, dependent: :destroy
   has_one_attached :avatar
 
+
   validates_presence_of :avatar, allow_blank: true   #allow_blank: trueで空の入力（値無し）を許可する
 
-  # validates :name, :email, :password, presence: true
-  # validates :name,
-  #   presence: {
-  #     message: "名前を入力してください"
-  #   }
-  #   validates :name,
-  #   uniqueness: {
-  #     message: "既にアカウントが存在します"
-  #   }
-  #   validates :email,
-  #   presence: {
-  #     message: "メールアドレスを入力してください"
-  #   }
-  #   validates :email,
-  #   uniqueness: {
-  #     message: "既に使用されているメールアドレスです"
-  #   }
-  #   validates :password,
-  #   presence: true,
-  #   length: {
-  #     minimum: 6,
-  #     too_short: "パスワードは6文字以上です"
-  #   }
-  #   validates :password,
-  #   uniqueness: {
-  #     message: "既に使用されているパスワードです"
-  #   }
+  validates :name, presence: { message: "入力してください" }
+  validates :name, uniqueness: { message: "既にアカウントが存在します" }
+  validates :name, length: { maximum: 50, message: "50文字以内で入力してください" }
+
+  validates :email, presence: { message: "入力してください" }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX, message: "無効なメールアドレスです" }
+  validates :email, uniqueness: { message: "既に存在するメールアドレスです" }
+
+  # validates :password, presence: { message: "入力してください" }
+  # validates :password, uniqueness: { message: "既に存在するパスワードです" }
+  validates :password, length: { minimum: 6, message: "6文字以上で入力してください" }
 end
