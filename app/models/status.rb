b2805c82d5de9efea7a230d40fc7b5c5aa385462
kurashi293 <my_post_office@icloud.com>
2020-05-14 13:34:02 +0900

    new, editのviewにエラーメッセージを追加
    バリデーションではじかれたカラムに対して、エラーメッセージを表示するように実装した

--- app/models/status.rb
+++ app/models/status.rb
@@ -15,9 +15,9 @@ class Status < ApplicationRecord
 
 
   validates :user_id, uniqueness: { message: "既に作成されたユーザーです" }
-  validates :user_id, :transceiver_id, :meal_id, :wear_id, :rank_id,  presence: true, numericality: { only_integer: true }
-  validates :transceiver_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 2 }
-  validates :meal_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9 }
-  validates :wear_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9 }
-  validates :rank_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 6 }
+  validates :user_id, :transceiver_id, :meal_id, :wear_id, :rank_id,  presence: { message: "選択してください" }, numericality: { only_integer: true, message: "無効な値です" }
+  validates :transceiver_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 2, message: "有効な値を選択してください" }
+  validates :meal_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9, message: "有効な値を選択してください" }
+  validates :wear_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 9, message: "有効な値を選択してください" }
+  validates :rank_id, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 6, message: "有効な値を選択してください" }
 end
