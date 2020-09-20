8251de3ed3ea528e35a2a5f5001ac5d6ca409973
kurashi293 <my_post_office@icloud.com>
2020-09-20 11:54:52 +0900

    tasks/editのviewをレスポンシブ仕様に変更
    PC、タブレット、スマートフォン
    三種類の画面サイズに対応させた。

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,7 @@ class Task < ApplicationRecord
 
 
   validates :title, :content, :deadline, :category_id, :user_ids, presence: { message: "入力してください" }
-  validates :title, length: { maximum: 100, message: "100文字以内で入力してください" }
+  validates :title, length: { maximum: 50, message: "50文字以内で入力してください" }
   validates :content, length: { maximum: 1200, message: "1200文字以内で入力してください" }
   validates :category_id, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3, message: "有効な値を選択してください" }
 end
