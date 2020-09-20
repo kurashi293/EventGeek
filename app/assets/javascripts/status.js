6685ea26db12a2a008a651979b54896d1f709660
kurashi293 <my_post_office@icloud.com>
2020-09-20 18:33:54 +0900

    statuses/indexのviewをレスポンシブ仕様に変更
    PC、タブレット、スマートフォン
    三種類の画面サイズに対応させた。

--- app/assets/javascripts/status.js
+++ app/assets/javascripts/status.js
@@ -1,32 +1,11 @@
-$(document).on('turbolinks:load', function(){
-  //tabをクリックしたときの動作
-  $('.tab li').click(function(){
-    //クリックしたタブのindexを取得
-    var index = $('.tab li').index(this);
+// タブを切り替える処理
+$(document).on("click", '.status-tab li', function(){
+  //クリックしたタブのindexを取得
+  var index = $('.status-tab li').index(this);
 
-    $('.contents .content').hide().removeClass('active');
-    $('.contents .content').eq(index).fadeIn().addClass('active');
+  $('.status-table ').hide().removeClass('active');
+  $('.status-table ').eq(index).show().addClass('active');
 
-    $('.tab li').removeClass('active');
-    $(this).addClass('active');
-  });
+  $('.status-tab li').removeClass('active');
+  $(this).addClass('active');
 });
-
-
-
-// $(document).on('scroll', 'window', function(){
-//   if (window.location.href.match(/\/groups\/\d+\/statuses/)) {
-//     $(function() {
-//       var topBtn = $("#scroll");
-//       topBtn.hide();
-//       //スクロールが100に達したらボタン表示
-//       $(window).scroll(function () {
-//         if ($(this).scrollTop() > 20) {
-//           topBtn.fadeIn();
-//         } else {
-//           topBtn.fadeOut();
-//         }
-//       });
-//     });
-//   };
-// });
