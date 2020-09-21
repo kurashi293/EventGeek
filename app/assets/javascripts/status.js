cc316da560fdb56ef78ffc0007b5fe210c591d87
kurashi293 <my_post_office@icloud.com>
2020-09-21 10:50:43 +0900

    statuses/new、statuses/editのviewをレスポンシブ仕様に変更
    PC、タブレット、スマートフォン
    三種類の画面サイズに対応させた。
    
    form部分のviewをパーシャルテンプレートにすることで、コードを管理しやすくした。

--- app/assets/javascripts/status.js
+++ app/assets/javascripts/status.js
@@ -15,7 +15,7 @@ $(document).on("click", '.status-tab li', function(){
 
 
 // 要素を拡大・縮小する処理
-$(window).on("load", function(){
+$(document).on('turbolinks:load', function(){
   $('.zoom75').click(function() {
       $(".table-wrap").css("zoom","75%");
       $("#current-user__row").css("border", "solid 0.4rem #e5a323")
@@ -31,4 +31,15 @@ $(window).on("load", function(){
       $(".table-wrap").css("zoom","125%");
       return false
   });
+});
+
+
+
+
+
+// スクリーンサイズが769px以下ならば、.date-selectに改行を適用させる処理
+$(document).on('turbolinks:load', function(){
+  if (window.matchMedia( '(max-width: 769px)' ).matches) {
+    $(".working-hour-hide-br").show();
+  }
 });
