f76e724831efeaf7dfce572e54ead914011d9aa8
kurashi293 <my_post_office@icloud.com>
2020-09-21 09:50:27 +0900

    拡大・縮小機能を追加
    ユーザーが任意で表を拡大・縮小出来ることで、モバイルデバイスが小さくなった場合でも全体的・局所的にストレス無くviewを閲覧することができるため。

--- app/assets/javascripts/status.js
+++ app/assets/javascripts/status.js
@@ -9,3 +9,26 @@ $(document).on("click", '.status-tab li', function(){
   $('.status-tab li').removeClass('active');
   $(this).addClass('active');
 });
+
+
+
+
+
+// 要素を拡大・縮小する処理
+$(window).on("load", function(){
+  $('.zoom75').click(function() {
+      $(".table-wrap").css("zoom","75%");
+      $("#current-user__row").css("border", "solid 0.4rem #e5a323")
+      return false
+  });
+
+  $('.zoom100').click(function() {
+      $(".table-wrap").css("zoom","100%");
+      return false
+  });
+
+  $('.zoom125').click(function() {
+      $(".table-wrap").css("zoom","125%");
+      return false
+  });
+});
