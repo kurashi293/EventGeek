c94f73378531ebff783d689425b5cf8b16a74750
kurashi293 <my_post_office@icloud.com>
2020-09-22 16:21:03 +0900

    スクロールでタブを画面上部に固定する処理を実装
    お知らせが多くなった場合、タブを切り替えるには画面最上部までスクロールして戻らなければいけない。
    
    これではユーザビリティー上良いとは言えないので、画面最上部からスクロールした数値がタブの座標数値より大きかった場合、タブを画面上部に固定する機能を実装した。

--- app/assets/javascripts/group/information.js
+++ app/assets/javascripts/group/information.js
@@ -9,3 +9,32 @@ $(document).on("click", '.information-tabs li', function(){
   $('.information-tabs li').removeClass('active');
   $(this).addClass('active');
 });
+
+
+
+
+
+// スクロールでタブを固定
+$(document).on('turbolinks:load', function(){
+  if (window.location.href.match(/\/groups\/\d+\/information/)) {
+    $(".content:last-child").css("border-bottom", "none");
+
+    $(function() {
+      // offset()で画面上からのtopとleftの座標を取得
+      var offset = $('.information-tabs').offset();
+
+      $(window).scroll(function () {
+        console.log("good");
+        // 画面最上部からスクロールした数値（topの座標）が、$('.information-tabs')のtop座標より大きかった場合の処理
+        if ($(window).scrollTop() > offset.top) {
+          $(".information-tabs").addClass("nav-fixed");
+          return false;
+
+        } else {
+          $(".information-tabs").removeClass("nav-fixed");
+          return false;
+        }
+      });
+    });
+  }
+});
