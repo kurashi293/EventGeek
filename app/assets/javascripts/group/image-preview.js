2891f540a970bcf890c0abf393c5d2fe3ddeca2c
kurashi293 <my_post_office@icloud.com>
2020-09-23 11:25:10 +0900

    グループフォームの画像プレビュー機能を修正
    主な変更点を下記に示す
    
    ・ターボリンクスの不要な読み込みを廃止
    
    ・選択されたファイルの情報がnullか空文字の場合処理を中止
    
    ・changeイベントが発火した時、ラベル要素の中にイメージ要素が既にあった場合、画像をプレビューするための要素
     $(".group_preview_box")を削除
    
    ・編集画面では、既にアップロード済みの画像をデフォルトで表示するように、条件分岐を用いてviewを変更

---
+++ app/assets/javascripts/group/image-preview.js
@@ -0,0 +1,92 @@
+// 選択画像のプレビュー
+
+// 前提知識
+// FileReader オブジェクトは、ユーザーのコンピューター内にあるファイルをウェブアプリケーションから非同期的に読み込むことが出来るWeb Api
+// attr => 属性の存在を確認する
+// prop => 属性の状態を確認する
+$(document).on('change', ".group-input-file", function(){
+
+  // 登録済みアバターに表示されているRe-selectの要素
+  $(".group-image-paragraph").remove();
+
+      // 変数file_dataは、選択されたファイルの情報群。 [0]はFileReader の状態を表す（EMPTY => まだデータは何も読み込まれていない状態）
+  var file_data = $(this).prop('files')[0],
+
+      // 変数filereaderは新しいfilereaderを作成する
+      filereader = new FileReader(),
+
+      // 変数labelは、イベントが起こった親要素（group-image-label）
+      label = $(this).parent(".group-image-label"),
+
+      // 変数find_imgは、イベントが起こった親要素（group-image-label）に存在するイメージ要素
+      find_img = $(this).parent().find('img');
+
+
+
+  // !は否定。偽の時に真を返し、真の時に偽を返す。
+  // 選択されたファイルの情報がnullか空文字ならtrueと判定。処理を中断。
+  if(!file_data){
+    // console.log("情報がnull");
+    return false;
+  }
+
+
+
+  // console.log(file_data);
+  // 選択されたファイル情報のtype: 項目にimageという文字列が含まれているかを判定。
+  // 含まれていなければアラートを出し、処理を中断する。
+  if(file_data.type.indexOf("image") < 0){
+    alert("画像ファイルを指定してください。");
+    return false;
+  }
+
+
+
+  // changeイベントが発火した時、avatar-labelにimg要素が既にあったら
+  if(find_img.length){
+    //avatar-labelのimg要素の次以降の兄弟要素を削除
+    find_img.nextAll().remove();
+    // avatar-labelのimg要素を削除
+    find_img.remove();
+    $(".group_preview_box").remove();
+  }
+
+
+
+  // 画像をプレビューするためのボックスを作成
+  var preview_box = '<div class="group_preview_box"><img alt="" class="img_selected"><p class="img_paragraph"><a href="#" class="img_delete">Cancel</a></p></div>';
+
+  // 変数label（changeイベントが起こったgroup-image-label要素）の最後に変数preview_boxを追加する
+  label.append(preview_box);
+  // 変数label（changeイベントが起こったgroup-image-label要素）からクラス名がgroup-image-iconの要素を特定し、document上から消す
+  label.find("group-image-icon").hide();
+
+
+
+  // コンテンツ（画像）の読み込みが完了して、利用可能な状態になったら
+  filereader.onload = function() {
+    // 変数labelからimg要素を見つけ出し、読み込んだファイルに存在するimg要素のsrc属性を取得する
+    // 最後にfilereaderのresultプロパティーで、読み取り操作が完了したファイルの内容を返す
+    label.find('img').attr('src', filereader.result);
+
+    // preview_box_delete関数に処理を繋げる
+    preview_box_delete(label);
+  }
+
+  // readAsDataURLメソッドはユーザーによって指定されたファイルを読み込むために使用。
+  filereader.readAsDataURL(file_data);
+
+  return false;     //親要素へのイベント伝播を止める
+});
+
+
+
+// 画像削除
+function preview_box_delete(label){
+  label.find("a.img_delete").on('click',function(){
+    $(this).parent().find('input[type=file]').val('');
+    $(this).parent().find('.group_preview_box, br').remove();
+    $(this).remove();
+  });
+  return false;
+}
