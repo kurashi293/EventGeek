f70f6284d267c3f0670e8b8bf0e862b42337b38b
kurashi293 <my_post_office@icloud.com>
2020-09-23 10:10:41 +0900

    ユーザーフォームの画像プレビュー機能を修正
    主な変更点を下記に示す
    
    ・ターボリンクスの不要な読み込みを廃止
    
    ・選択されたファイルの情報がnullか空文字の場合処理を中止
    
    ・changeイベントが発火した時、ラベル要素の中にイメージ要素が既にあった場合、画像をプレビューするための要素
     $(".preview_box")を削除
    
    ・編集画面では、既にアップロード済みの画像をデフォルトで表示するように、条件分岐を用いてviewを変更

--- app/assets/javascripts/user-form.js
+++ app/assets/javascripts/user-form.js
@@ -2,64 +2,108 @@
 
 // 前提知識
 // FileReader オブジェクトは、ユーザーのコンピューター内にあるファイルをウェブアプリケーションから非同期的に読み込むことが出来るWeb Api
-// attr => 属性の存在有無だけを確認
+// attr => 属性の存在を確認する
 // prop => 属性の状態を確認する
-$(document).on('turbolinks:load', function(){
-  $(".input-file").on('change', function(){
+$(document).on('change', ".input-file", function(){
 
-        // 変数filepropは、FileReader の状態を表す数値0（EMPTY => まだデータは何も読み込まれていない状態）を確認する
-    var fileprop = $(this).prop('files')[0],
-        // 変数find_imgは、イベントが起こった親要素に存在するイメージ要素を見つける
-        find_img = $(this).parent().find('img'),
-        // 変数filereaderは新しいfilereaderを作成する
-        filereader = new FileReader(),
-        // 変数view_boxは、イベントが起こった親要素（avatar-label）
-        view_box = $(this).parent(".avatar-label");
+  // 登録済みアバターに表示されているRe-selectの要素
+  $(".avatar-paragraph").remove();
 
-    if(find_img.length){
-      find_img.nextAll().remove();
-      find_img.remove();
-    }
+      // 変数file_dataは、選択されたファイルの情報群。 [0]はFileReader の状態を表す（EMPTY => まだデータは何も読み込まれていない状態）
+  var file_data = $(this).prop('files')[0],
 
-    if(fileprop.type.indexOf("image") < 0){
-      alert("画像ファイルを指定してください。");
-      return false;
-    }
+      // 変数filereaderは新しいfilereaderを作成する
+      filereader = new FileReader(),
+
+      // 変数labelは、イベントが起こった親要素（avatar-label）
+      label = $(this).parent(".avatar-label"),
+
+      // 変数find_imgは、イベントが起こった親要素（avatar-label）に存在するイメージ要素
+      find_img = $(this).parent().find('img');
 
 
-    // 画像をプレビューするためのボックスを作成
-    var img = '<div class="preview_box"><img alt="" class="img_selected"><p><a href="#" class="img_delete"><i class="far fa-window-close" id="close-icon"></i></a></p></div>';
 
-    // 変数view_box（changeイベントが起こったavatar-label要素）の最後に変数imgを追加する
-    view_box.append(img);
-    // 変数view_box（changeイベントが起こったavatar-label要素）からクラス名がimage-iconの要素を特定し、document上から消す
-    view_box.find(".image-icon").hide();
-
-
-    // コンテンツ（画像）の読み込みが完了して、利用可能な状態になったら
-    filereader.onload = function() {
-
-      // 変数view_boxからimg要素を見つけ出し、読み込んだファイルに存在するimg要素のsrc属性を取得する
-      // 最後にfilereaderのresultプロパティーで、読み取り操作が完了したファイルの内容を返す
-      view_box.find('img').attr('src', filereader.result);
-      img_delete(view_box);
-    }
-
-    // readAsDataURLメソッドはユーザーによって指定されたファイルを読み込むために使用。
-    filereader.readAsDataURL(fileprop);
-
-    return false;     //親要素へのイベント伝播を止める
-  });
-
-
-  function img_delete(target){
-    target.find("a.img_delete").on('click',function(){
-      if(window.confirm('選択した画像を削除します。\nよろしいですか？')){
-        $(this).parent().find('input[type=file]').val('');
-        $(this).parent().find('.preview_box, br').remove();
-        $(this).remove();
-      }
-    });
+  // !は否定。偽の時に真を返し、真の時に偽を返す。
+  // 選択されたファイルの情報がnullか空文字ならtrueと判定。処理を中断。
+  if(!file_data){
+    // console.log("情報がnull");
     return false;
   }
+
+
+
+  // console.log(file_data);
+  // 選択されたファイル情報のtype: 項目にimageという文字列が含まれているかを判定。
+  // 含まれていなければアラートを出し、処理を中断する。
+  if(file_data.type.indexOf("image") < 0){
+    alert("画像ファイルを指定してください。");
+    return false;
+  }
+
+
+
+  // changeイベントが発火した時、avatar-labelにimg要素が既にあったら
+  if(find_img.length){
+    //avatar-labelのimg要素の次以降の兄弟要素を削除
+    find_img.nextAll().remove();
+    // avatar-labelのimg要素を削除
+    find_img.remove();
+    $(".preview_box").remove();
+  }
+
+
+
+  // 画像をプレビューするためのボックスを作成
+  var preview_box = '<div class="preview_box"><img alt="" class="img_selected"><p class="img_paragraph"><a href="#" class="img_delete">Cancel</a></p></div>';
+
+  // 変数label（changeイベントが起こったavatar-label要素）の最後に変数preview_boxを追加する
+  label.append(preview_box);
+  // 変数label（changeイベントが起こったavatar-label要素）からクラス名がimage-iconの要素を特定し、document上から消す
+  label.find(".image-icon").hide();
+
+
+
+  // コンテンツ（画像）の読み込みが完了して、利用可能な状態になったら
+  filereader.onload = function() {
+    // 変数labelからimg要素を見つけ出し、読み込んだファイルに存在するimg要素のsrc属性を取得する
+    // 最後にfilereaderのresultプロパティーで、読み取り操作が完了したファイルの内容を返す
+    label.find('img').attr('src', filereader.result);
+
+    // preview_box_delete関数に処理を繋げる
+    preview_box_delete(label);
+  }
+
+  // readAsDataURLメソッドはユーザーによって指定されたファイルを読み込むために使用。
+  filereader.readAsDataURL(file_data);
+
+  return false;     //親要素へのイベント伝播を止める
+});
+
+
+
+// 画像削除
+function preview_box_delete(label){
+  label.find("a.img_delete").on('click',function(){
+    $(this).parent().find('input[type=file]').val('');
+    $(this).parent().find('.preview_box, br').remove();
+    $(this).remove();
+  });
+  return false;
+}
+
+
+
+
+
+// CancelとRe-selectの切り替え
+$(document).on('mouseenter', '.img_selected', function(){
+  var html = `<div class="img_inform">Re-select</div>`;
+  $(".img_paragraph").append(html);
+  $(".img_delete").hide();
+});
+
+
+$(document).on('mouseleave','.img_selected',function(){
+  $(".img_inform").remove();
+  $(".img_delete").show();
 });
