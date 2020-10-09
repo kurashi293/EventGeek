8c934940fb5d787696be27e64e984aca37a2e48e
kurashi293 <my_post_office@icloud.com>
2020-10-09 18:03:31 +0900

    [add] gem 'rails-i18n'の導入と設定
    言語の文言を別の言語の文言に翻訳してくれるgemを追加。
    
    日本語の翻訳ファイルであるja.ymlを参照するように設定。

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,14 @@ module EventGeek
     # 下記の記述はfield_with_errorsクラスを読み込まないようにする
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
 
+
+    # バリデーション失敗時に、日本語のエラーメッセージを使用する設定
+    # デフォルトはen.ymlを参照してしまうので、ja.ymlを参照するように設定
+    config.i18n.default_locale = :ja
+
+    # config/locales/以下に設定した翻訳ファイルが全て読み込まれるように、以下のコードを設定。
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
